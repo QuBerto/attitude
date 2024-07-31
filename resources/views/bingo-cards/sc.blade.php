7c0dc94a9abd27d00a51e041aa2ca776b442dc54
QuBerto <roberto@guagliardo.nl>
2024-07-31 23:15:19 +0200

    Fix

--- resources/views/bingo-cards/sc.blade.php
+++ resources/views/bingo-cards/sc.blade.php
@@ -12,7 +12,9 @@
         <label for="channel" class="block text-sm font-medium mb-2">Channel:</label>
         <select id="channel" name="channel" class="block w-full px-3 py-2 border border-gray-300 dark:border-gray-700 bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-200 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500">
             @foreach ($channels as $channel)
+                @isset($channel['name'])
                 <option value="{{ $channel['id'] }}">{{ $channel['name'] }}</option>
+                @endisset
             @endforeach
         </select>
     </div>
