61f1e1f547328544eabb8156b4ad9217b11731ad
QuBerto <roberto@guagliardo.nl>
2024-08-06 01:15:43 +0200

    Update completion

--- resources/views/frontend/bingo/drops.blade.php
+++ resources/views/frontend/bingo/drops.blade.php
@@ -6,7 +6,7 @@
 <table class="min-w-full divide-y divide-gray-300">
     <thead>
       <tr>
-        <th scope="col" class="py-3.5 pl-4 pr-3 text-left  font-semibold sm:pl-0 rs-yellow">Tile</th>
+        <th scope="col" colspan="2" class="py-3.5 pl-4 pr-3 text-left  font-semibold sm:pl-0 rs-yellow">Tile</th>
         <th scope="col" class="px-3 py-3.5 text-left  font-semibold rs-yellow">Item</th>
         <th scope="col" class="px-3 py-3.5 text-left  font-semibold rs-yellow" >User</th>
         
@@ -17,7 +17,8 @@
       @isset($team)
       @foreach($team->completions as $completion)
       <tr>
-        <td class="whitespace-nowrap px-3 py-4  text-white">{{ $completion->task->description }}
+        <td class="whitespace-nowrap px-3 py-4  text-white"><div style="max-height:50px;width:auto;" class="flex center"><img style="max-height:50px;width:auto;" src="{{$completion->task->tile->getFirstMediaUrl("*")}}"></div></td>
+        <td class="whitespace-nowrap px-3 py-4  text-white">{{ $completion->task->description }}<br><small>{{ $completion->task->tile->title }}</small>
        
         <td class="whitespace-nowrap px-3 py-4  text-white">{{$completion->description}}</td>
  
