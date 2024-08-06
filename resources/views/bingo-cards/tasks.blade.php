f4da3308d74510cd4f52c3f4d47586a3d5b462c5
QuBerto <roberto@guagliardo.nl>
2024-08-07 00:48:24 +0200

    REsponsive table

--- resources/views/bingo-cards/tasks.blade.php
+++ resources/views/bingo-cards/tasks.blade.php
@@ -8,7 +8,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
+                <div class="p-6 text-gray-900 dark:text-gray-100 overflow-x-auto">
                     <div class="flex gap-5">
                     @foreach($allteams as $allteam)
                         <h2><a href="{{route('tasks.team', ['team' => $allteam->id, 'bingo' => "1"])}}">{{$allteam->name}}</a></h2>
