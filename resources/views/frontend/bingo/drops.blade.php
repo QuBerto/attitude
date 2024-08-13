f7b7a51d7b5fe326707164256ca284158886d9f3
QuBerto <roberto@guagliardo.nl>
2024-08-13 23:47:49 +0200

    fix drops

--- resources/views/frontend/bingo/drops.blade.php
+++ resources/views/frontend/bingo/drops.blade.php
@@ -1,41 +1,44 @@
-@extends('layouts.frontend')
-@section('content')
-    <h1 class="text-3xl font-bold mb-6">Drops Index</h1>
-
-    @if($drops->isEmpty())
-        <p class="text-white">No drops found.</p>
-    @else
-        <div class="overflow-x-auto">
-            <table class="min-w-full divide-y divide-gray-300">
-                <thead>
-                    <tr>
-                        <th scope="col" class="py-3.5 pl-4 pr-3 text-left font-semibold sm:pl-0 text-yellow-500">Player</th>
-                        <th scope="col" class="px-3 py-3.5 text-left font-semibold text-yellow-500">Event Code</th>
-                        <th scope="col" class="px-3 py-3.5 text-left font-semibold text-yellow-500">Item Source</th>
-                        <th scope="col" class="px-3 py-3.5 text-left font-semibold text-yellow-500">Items</th>
-                        <th scope="col" class="px-3 py-3.5 text-left font-semibold text-yellow-500">Value</th>
-                        <th scope="col" class="px-3 py-3.5 text-left font-semibold text-yellow-500">Time</th>
-                    </tr>
-                </thead>
-                <tbody class="divide-y divide-gray-800">
-                    @foreach($drops as $drop)
-                        <tr>
-                            <td class="whitespace-nowrap px-3 py-4 text-white">{{ $drop->player->username }}</td>
-                            <td class="whitespace-nowrap px-3 py-4 text-white">{{ $drop->eventcode }}</td>
-                            <td class="whitespace-nowrap px-3 py-4 text-white">{{ $drop->itemsource }}</td>
-                            <td class="whitespace-nowrap px-3 py-4 text-white">
-                                <ul class="list-disc list-inside">
-                                    @foreach($drop->items as $item)
-                                        <li>{{ $item['name'] }} (x{{ $item['quantity'] }})</li>
-                                    @endforeach
-                                </ul>
-                            </td>
-                            <td class="whitespace-nowrap px-3 py-4 text-white">{{ $drop->gp }} GP</td>
-                            <td class="whitespace-nowrap px-3 py-4 text-white">{{ $drop->created_at }}</td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
-    @endif
-@stop
+@if($bingoCard->teams)
+<h2 class="font-semibold text-xl leading-tight rs-yellow">
+    {{ __('Completions') }}
+</h2>
+<div class="overflow-x-auto">
+  <table class="min-w-full divide-y divide-gray-300">
+      <thead>
+        <tr>
+          <th scope="col" colspan="2" class="py-3.5 pl-4 pr-3 text-left font-semibold sm:pl-0 rs-yellow">Tile</th>
+          <th scope="col" class="px-3 py-3.5 text-left font-semibold rs-yellow">Item</th>
+          <th scope="col" class="px-3 py-3.5 text-left font-semibold rs-yellow">User</th>
+        </tr>
+      </thead>
+      <tbody class="divide-y divide-gray-800">
+        @isset($team)
+        @foreach($team->completions as $completion)
+        <tr>
+          <td class="whitespace-nowrap px-3 py-4 text-white">
+            <div class="flex justify-center">
+              <img class="max-h-12 w-auto" src="{{$completion->task->tile->getFirstMediaUrl('*')}}" alt="{{ $completion->task->tile->title }}">
+            </div>
+          </td>
+          <td class="whitespace-nowrap px-3 py-4 text-white">{{ $completion->task->description }}<br>
+            <small>{{ $completion->task->tile->title }}</small>
+          </td>
+          <td class="whitespace-nowrap px-3 py-4 text-white">{{ $completion->description }}</td>
+          <td class="whitespace-nowrap px-3 py-4 text-white">{{ $completion->user->nick }}</td>
+        </tr>
+        @endforeach
+        @else
+        @foreach($bingoCard->teams[0]->completions as $completion)
+        <tr>
+          <td class="whitespace-nowrap px-3 py-4 text-white">
+            {{ $completion->task->description }}
+          </td>
+          <td class="whitespace-nowrap px-3 py-4 text-white">{{ $completion->description }}</td>
+          <td class="whitespace-nowrap px-3 py-4 text-white">{{ $completion->user->username }}</td>
+        </tr>
+        @endforeach
+        @endisset
+      </tbody>
+    </table>
+</div>
+@endif
