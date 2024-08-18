36d25888cf410050fba675b648b3466e812253dc
QuBerto <roberto@guagliardo.nl>
2024-08-18 14:33:58 +0200

    fix teamdata

--- resources/views/frontend/bingo/boss.blade.php
+++ resources/views/frontend/bingo/boss.blade.php
@@ -1,8 +1,8 @@
 
 @php
-if(!isset($teamData)){
-    return;
-}
+// if(!isset($teamData)){
+//     return;
+// }
     // Group tiles by their bosses
     $groupedTiles = [];
     foreach ($bingoCard->tiles as $tile) {
