e59d63053e25b70265f720c2621bd38f5df85ed9
QuBerto <roberto@guagliardo.nl>
2024-09-14 18:43:33 +0200

    fix kills value

---
+++ app/Console/Commands/fixKills.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\NpcKill;
+class fixKills extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:fix-kills';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $kills = NpcKill::all();
+        foreach($kills as $kill){
+            $value = 0;
+            foreach($kill->items as $item){
+                if ($item->osrsItem){
+                    if ($item->osrsItem->value > 0){
+                        $value += $item->osrsItem->value;
+                    }
+                }
+            }
+            $kill->ge_price = $value;
+            $kill->save();
+        }
+    }
+}
