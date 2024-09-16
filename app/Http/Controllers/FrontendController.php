3d3f3bcdcd7963a1ef64b54bd0ef5fcca48e98d3
your name <roberto@guagliardo.nl>
2024-09-16 19:47:20 +0200

    fix frontend

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -36,20 +36,24 @@ class FrontendController extends Controller
         // Get the current month and year
         $currentMonth = Carbon::now()->month;
         $currentYear = Carbon::now()->year;
-
-        // Fetch the top 10 NPC kills by ge_price for the current month
-        $kills = NpcKill::whereYear('created_at', $currentYear)
-                    ->whereMonth('created_at', $currentMonth)
-                    ->orderBy('ge_price', 'desc')
-                    ->limit(10)
-                    ->get();
+        $kills = NpcKill::whereIn('id', function ($query) use ($currentYear, $currentMonth) {
+            $query->select(DB::raw('MAX(id)'))
+                ->from('npc_kills')
+                ->whereYear('created_at', $currentYear)
+                ->whereMonth('created_at', $currentMonth)
+                ->groupBy('discord_user_id');
+        })
+        ->orderBy('ge_price', 'desc')
+        ->limit(10)
+        ->get();
+        
 
         $totalLootPerUser = NpcKill::select('discord_user_id', DB::raw('SUM(ge_price) as total_loot'))
                     ->whereYear('created_at', $currentYear)
                     ->whereMonth('created_at', $currentMonth)
                     ->groupBy('discord_user_id')
                     ->orderBy('total_loot', 'desc')
-                    ->distinct('discord_user_id')
+                    
                     ->limit(10)
                     ->get();
                 
