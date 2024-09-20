d2788f4302662006519d99794025c5ca578418c6
your name <roberto@guagliardo.nl>
2024-09-20 18:32:11 +0200

    Update channels

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -51,54 +51,68 @@ class WebhookController extends Controller
         if ($type){
             Log::info('Request type:', [$type], true);
         }
-
+        $channels = [
+        
+                'DEATH' => 1059049971783061524,
+                'LEVEL' => 1059051291663413248,
+                'XP_MILESTONE' => 1059051291663413248,
+                'COLLECTION' => 1059050475166650408,
+                'LOOT'  => 1063869073219403866,
+                'CLUE' => 1059050187382865970,
+                'COMBAT_ACHIEVEMENT' => 1059050475166650408,
+                'ACHIEVEMENT_DIARY' => 1059050475166650408,
+                'PET' => 1059050146400309248,
+                'BARBARIAN_ASSAULT_GAMBLE' => 1063869073219403866,
+                'PLAYER_KILL' => 1063869073219403866,
+            ];
         
         switch($type){
             case 'DEATH':
-                $channel_id = 1059049971783061524;
+                $channel_id = $channels['DEATH'];
                 break;
             case 'LEVEL':
-                $channel_id = 1059051291663413248;
+                $channel_id = $channels['LEVEL'];
                 break;
             case 'XP_MILESTONE':
-                $channel_id = 1059051291663413248;
+                $channel_id = $channels['LEVEL'];
                 break;
             case 'COLLECTION':
-                $channel_id = 1059050475166650408;
+                $channel_id = $channels['COLLECTION'];
                 break;
             case 'LOOT':
                 if ($extra['category'] == 'NPC'){
                     $npc = new NpcKillController();
                     $npc->storeNew($extra, $player, $user);
                 }
-                $channel_id = 1063869073219403866;
+                $channel_id = $channels['LOOT'];
                 break;
             case 'SLAYER':
+                $channel_id = $channels['COLLECTION'];
                 break;
             case 'QUEST':
-                $channel_id = 1063869073219403866;
+                $channel_id = $channels['QUEST'];
                 break;
             case 'CLUE':
-                $channel_id = 1059050187382865970;
+                $channel_id = $channels['CLUE'];
                 break;
             case 'KILL_COUNT':
                 break;
             case 'COMBAT_ACHIEVEMENT':
-                $channel_id = 1059050475166650408;
+                $channel_id = $channels['COLLECTION'];
                 break;
             case 'ACHIEVEMENT_DIARY':
-                $channel_id = 1059050475166650408;
+                $channel_id = $channels['COLLECTION'];
                 break;
             case 'PET':
-                $channel_id = 1059050146400309248;
+                $channel_id = $channels['PET'];
                 break;
             case 'SPEEDRUN':
                 break;
             case 'BARBARIAN_ASSAULT_GAMBLE':
-                $channel_id = 1063869073219403866;
+                $channel_id = $channels['BARBARIAN_ASSAULT_GAMBLE'];
                 break;
             case 'PLAYER_KILL':
-                $channel_id = 1063869073219403866;
+                $channel_id = $channels['PLAYER_KILL'];
                 break;
             case 'GRAND_EXCHANGE':
                 break;
@@ -119,7 +133,7 @@ class WebhookController extends Controller
                 $channel_id = 1107960124871544946;
                 break;
         }
-        $channel_id = 1107960124871544946;
+        //$channel_id = 1107960124871544946;
         // Process the request as needed
         // ...
         // Get the uploaded file (temporary file)
